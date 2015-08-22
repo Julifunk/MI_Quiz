598050f4a06669468883b415294f72baaf5cb5e0
mabeck92 <marcus-beck92@gmx.de>
2015-08-22 14:23:10 +0200

    first question with answers

--- Test/app/src/main/java/com/example/julia/test/MainActivity.java
+++ Test/app/src/main/java/com/example/julia/test/MainActivity.java
@@ -1,25 +1,25 @@
 package com.example.julia.test;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 import com.parse.FindCallback;
 import com.parse.Parse;
-import com.parse.ParseACL;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
     private Button eimi;
+    ParseObject object;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +27,9 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "BUfWzIhsninLF29zGCoEz7puv93amubRhTUmfY63", "L36y1hNcNkMI5qVmz8ytD8DsrqnSwrYzmi9gmCYM");
-        setContentView (R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        eimi = (Button) findViewById (R.id.button);
-        eimi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Ques.class);
-                startActivity(i);
-            }
-        });
+
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Binar_HexaOperationen");
         query.whereExists("question");
@@ -45,6 +38,19 @@ public class MainActivity extends ActionBarActivity {
             public void done(List<ParseObject> list, ParseException e) {
               if (list.size() > 0) {
                   for (int i = 0; i < list.size(); i++) {
+
+                      object = list.get(0);
+                      object.pinInBackground();
+
+                      eimi = (Button) findViewById (R.id.button);
+                      eimi.setOnClickListener(new View.OnClickListener() {
+                          @Override
+                          public void onClick(View v) {
+                              Intent i = new Intent(getApplicationContext(),Ques.class);
+                              i.putExtra("objectId", object.getObjectId());
+                              startActivity(i);
+                          }
+                      });
 
                       ParseObject p = list.get(i);
                       String question = p.getString("question");
--- Test/app/src/main/java/com/example/julia/test/Ques.java
+++ Test/app/src/main/java/com/example/julia/test/Ques.java
@@ -2,9 +2,15 @@ package com.example.julia.test;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.TextView;
 
-import com.example.julia.test.R;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by Julia on 19.08.2015.
@@ -17,17 +23,52 @@ public class Ques extends ActionBarActivity {
     private TextView option_c;
     private TextView option_d;
 
+    private ParseObject parseObject;
 
     @Override
     protected void onCreate (Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView (R.layout.activity_questions);
 
+        String objectId = getIntent().getStringExtra("objectId");
 
-        question = (TextView) findViewById(R.id.question);
-        option_a = (TextView) findViewById(R.id.option_a);
-        option_b = (TextView) findViewById(R.id.option_c);
-        option_d = (TextView) findViewById(R.id.option_d);
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Binar_HexaOperationen");
+        query.fromLocalDatastore();
+        query.getInBackground(objectId, new GetCallback<ParseObject>() {
+            @Override
+            public void done(final ParseObject parseObject, ParseException e) {
+
+                question = (TextView) findViewById(R.id.question);
+                option_a = (TextView) findViewById(R.id.option_a);
+                option_b = (TextView) findViewById(R.id.option_b);
+                option_c = (TextView) findViewById(R.id.option_c);
+                option_d = (TextView) findViewById(R.id.option_d);
+
+                option_d.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TextView response = (TextView) findViewById(R.id.response);
+                        if (option_d.getText().toString().equals(parseObject.getString("correctAnswer"))) {
+                            response.setText("Correct");
+                        }
+                    }
+                });
+
+                List answerOptions = parseObject.getList("answerOptions");
+                question.setText(parseObject.getString("question"));
+                option_a.setText(answerOptions.get(0).toString());
+                option_b.setText(parseObject.getList("answerOptions").get(1).toString());
+                option_c.setText(parseObject.getList("answerOptions").get(2).toString());
+                option_d.setText(parseObject.getList("answerOptions").get(3).toString());
+
+
+                parseObject.unpinInBackground();
+            }
+        });
+
+
+
+
     }
 
 
