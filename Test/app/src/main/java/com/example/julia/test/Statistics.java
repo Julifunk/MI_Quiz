be95ba43a1ecf3f582362cf6ccae0496db0ec9a0
Philip Eiermann <philip.eiermann@googlemail.com>
2015-09-06 17:58:38 +0200

    progressBar

--- Test/app/src/main/java/com/example/julia/test/Statistics.java
+++ Test/app/src/main/java/com/example/julia/test/Statistics.java
@@ -1,15 +1,41 @@
 package com.example.julia.test;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.os.Handler;
+
+
 /**
  * Created by Julia on 06.09.2015.
  */
-public class Statistics {
-    private static Statistics ourInstance = new Statistics();
+public class Statistics extends Activity {
 
-    public static Statistics getInstance() {
-        return ourInstance;
+    private ProgressBar quesProgress;
+    private int mProgressStatus = 0;
+    private Handler myHandler = new Handler();
+
+    @Override
+    protected void onCreate (Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.statistics);
+
+        quesProgress = (ProgressBar) findViewById(R.id.progressBar);
+
+       /*new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 15) {
+
+                    // Update the progress bar
+                    myHandler.post(new Runnable() {
+                        public void run() {
+                            quesProgress.setProgress(mProgressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();*/
+
     }
 
-    private Statistics() {
-    }
 }
