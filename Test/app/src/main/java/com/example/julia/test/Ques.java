bf8e322768c187a26bef54e24abbedef2cfdbb8a
Julifunk <julia@groetsch.org>
2015-09-11 00:23:48 +0200

    PowPowPow

--- Test/app/src/main/java/com/example/julia/test/Ques.java
+++ Test/app/src/main/java/com/example/julia/test/Ques.java
@@ -12,6 +12,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -30,6 +32,7 @@ public class Ques extends ActionBarActivity implements QuestionsDataProvider.Que
     private int counterOfCorrectAnswers;
     private String subject;
     private String set;
+    private View.OnClickListener cL;
 
     ArrayList<QuestionsObject> questions = new ArrayList<>();
     Random rGen = new Random();
@@ -48,10 +51,16 @@ public class Ques extends ActionBarActivity implements QuestionsDataProvider.Que
         subject = getIntent().getStringExtra("subject");
         set = getIntent().getStringExtra("set");
 
+        setupDataProvider();
+        handleUserInput();
+
+
+    }
+
+    private void setupDataProvider() {
         questionsDataProvider = new QuestionsDataProvider();
         questionsDataProvider.setQuestionsDataProviderListener(this);
         questionsDataProvider.getQuestionsFromSubjectAndSet(subject, set);
-
     }
 
     private void getUIReferences() {
@@ -60,104 +69,85 @@ public class Ques extends ActionBarActivity implements QuestionsDataProvider.Que
         option_b = (TextView) findViewById(R.id.option_b);
         option_c = (TextView) findViewById(R.id.option_c);
         option_d = (TextView) findViewById(R.id.option_d);
+    }
 
+   //will detect whick answer User has clicked and if it if right or wrong
+    private void handleUserInput() {
 
-
-        option_a.setOnClickListener(new View.OnClickListener() {
+        cL = new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
             @Override
+            public void onClick(View v) {
+                if (v.equals(option_a)) {
+                    if (isCorrectAnswer(option_a.getText().toString())) {
 
-            public void onClick(View view) {
-                if (isCorrectAnswer(option_a.getText().toString())) {
+                        counterOfCorrectAnswers++;
+                        option_a.setBackground(getDrawable(R.drawable.progress_bar_positive));
+                    } else {
 
-                    counterOfCorrectAnswers++;
-                    option_a.setBackground(getDrawable(R.drawable.progress_bar_positive));
+                        option_a.setBackground(getDrawable(R.drawable.progress_bar_negative));
                     }
-                else{
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
 
-                    option_a.setBackground(getDrawable(R.drawable.progress_bar_negative));
+                            option_a.setBackgroundColor(Color.WHITE);
+                            currentQuestion++;
+                            setTextFieldWithQuestionAtIndex();
+                            counterOfAnsweredQuestions++;
+                        }
+                    }, 500);
                 }
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        option_a.setBackgroundColor(Color.WHITE);
-                        currentQuestion++;
-                        setTextFieldWithQuestionAtIndex();
-                        counterOfAnsweredQuestions++;
+                if (v.equals(option_b)) {
+                    if (isCorrectAnswer(option_b.getText().toString())) {
+
+                        counterOfCorrectAnswers++;
+                        option_b.setBackground(getDrawable(R.drawable.progress_bar_positive));
                     }
-                }, 500);
-
-            }
-        });
-
-
-        option_b.setOnClickListener(new View.OnClickListener() {
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onClick(View view) {
-                if (isCorrectAnswer(option_b.getText().toString())) {
-
-                    counterOfCorrectAnswers++;
-                    option_b.setBackground(getDrawable(R.drawable.progress_bar_positive));
-                }
-                else{
-                    option_b.setBackground(getDrawable(R.drawable.progress_bar_negative));
-                }
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        option_b.setBackgroundColor(Color.WHITE);
-                        currentQuestion++;
-                        setTextFieldWithQuestionAtIndex();
-                        counterOfAnsweredQuestions++;
+                    else{
+                        option_b.setBackground(getDrawable(R.drawable.progress_bar_negative));
                     }
-                }, 500);
-
-            }
-        });
-
-        option_c.setOnClickListener(new View.OnClickListener(){
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            @Override
-
-            public void onClick(View view) {
-                if (isCorrectAnswer(option_c.getText().toString())) {
-
-                    counterOfCorrectAnswers++;
-                    option_c.setBackground(getDrawable(R.drawable.progress_bar_positive));
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            option_b.setBackgroundColor(Color.WHITE);
+                            currentQuestion++;
+                            setTextFieldWithQuestionAtIndex();
+                            counterOfAnsweredQuestions++;
+                        }
+                    }, 500);
                 }
-                else{
-                    option_c.setBackground(getDrawable(R.drawable.progress_bar_negative));
-                }
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        option_c.setBackgroundColor(Color.WHITE);
-                        currentQuestion++;
-                        setTextFieldWithQuestionAtIndex();
-                        counterOfAnsweredQuestions++;
+                if (v.equals(option_c)) {
+                    if (isCorrectAnswer(option_c.getText().toString())) {
+
+                        counterOfCorrectAnswers++;
+                        option_c.setBackground(getDrawable(R.drawable.progress_bar_positive));
                     }
-                }, 500);
-
-            }
-        });
-
-        option_d.setOnClickListener(new View.OnClickListener() {
-
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            @Override
-
-            public void onClick(View view) {
-                if (isCorrectAnswer(option_d.getText().toString())) {
-
-                    counterOfCorrectAnswers++;
-                    option_d.setBackground(getDrawable(R.drawable.progress_bar_positive));
+                    else{
+                        option_c.setBackground(getDrawable(R.drawable.progress_bar_negative));
+                    }
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            option_c.setBackgroundColor(Color.WHITE);
+                            currentQuestion++;
+                            setTextFieldWithQuestionAtIndex();
+                            counterOfAnsweredQuestions++;
+                        }
+                    }, 500);
                 }
-                else{
-                    option_d.setBackground(getDrawable(R.drawable.progress_bar_negative));
+                if (v.equals(option_d)) {
+                    if (isCorrectAnswer(option_d.getText().toString())) {
+
+                        counterOfCorrectAnswers++;
+                        option_d.setBackground(getDrawable(R.drawable.progress_bar_positive));
+                    }
+                    else{
+                        option_d.setBackground(getDrawable(R.drawable.progress_bar_negative));
+                    }
                 }
                 final Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
@@ -169,13 +159,18 @@ public class Ques extends ActionBarActivity implements QuestionsDataProvider.Que
                         counterOfAnsweredQuestions++;
                     }
                 }, 500);
-
             }
-        });
-
+        };
+        option_a.setOnClickListener(cL);
+        option_b.setOnClickListener(cL);
+        option_c.setOnClickListener(cL);
+        option_d.setOnClickListener(cL);
 
     }
 
+
+
+    //gets a random Question from ArrayList<QuestionsObject> and removes it after usage
     private void setTextFieldWithQuestionAtIndex() {
         if (questions.size() < 1) {
             Intent i = new Intent(getApplicationContext(), Statistics.class);
@@ -185,25 +180,30 @@ public class Ques extends ActionBarActivity implements QuestionsDataProvider.Que
             startActivity(i);
             return;
         }
-        rGen = new Random();
 
+        rGen = new Random();
         if(this.questions.size()>0) {
             randomIndex = rGen.nextInt(this.questions.size());
         }
+
         QuestionsObject currentQuestion = this.questions.get(randomIndex);
+        displayQuestion(currentQuestion);
+        questions.remove(randomIndex);
+
+
+
+       }
+
+    //displays the current question
+    private void displayQuestion(QuestionsObject currentQuestion) {
         question.setText(currentQuestion.getQuestion());
         option_a.setText(currentQuestion.getOptionA());
         option_b.setText(currentQuestion.getOptionB());
         option_c.setText(currentQuestion.getOptionC());
         option_d.setText(currentQuestion.getOptionD());
         correctAnswer = currentQuestion.getCorrectAnswer();
-        System.out.println("Korrekte Antwort von currentQuestion:       " + correctAnswer);
-        questions.remove(randomIndex);
-        System.out.println(this.questions.size());
+
     }
-
-
-
 
 
     private boolean isCorrectAnswer(String selectedAnswer) {
@@ -218,6 +218,9 @@ public class Ques extends ActionBarActivity implements QuestionsDataProvider.Que
         System.out.println(this.questions.size());
         setTextFieldWithQuestionAtIndex();
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
